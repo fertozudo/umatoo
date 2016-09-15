6ab773a83ba3a104fa39614407d7f3c49992bfad
fertozudo <plasticoestocastico@gmail.com>
2016-09-16 01:17:04 +0200

    First commit and target model

--- savings/admin.py
+++ savings/admin.py
@@ -1,10 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
-from savings.models import Target
+from django.contrib.auth.models import User
+
+from savings.models import Target, ProfileUser
 
 
-class TargetAdmin(admin.ModelAdmin):
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileUserInline(admin.StackedInline):
+    model = ProfileUser
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileUserInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+class SavingsBaseModelAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return super(admin.ModelAdmin, self).get_queryset(request)
+        else:
+            return super(admin.ModelAdmin, self).get_queryset(request).filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        """When creating a new object, set the owner field.
+        """
+        if not change:
+            obj.owner = request.user
+        obj.save()
+
+
+class TargetAdmin(SavingsBaseModelAdmin):
     list_display = ('name', 'amount', 'days')
 
 admin.site.register(Target, TargetAdmin)
--- savings/models.py
+++ savings/models.py
@@ -5,11 +5,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Target(models.Model):
+class ProfileUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateTimeField()
+    gender = models.MultipleChoiceField(choices=(('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')))
+
+
+class SavingsBaseModel(models.Model):
+    owner = models.ForeignKey(User, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Target(SavingsBaseModel):
     name = models.CharField(max_length=100)
     amount = models.IntegerField(default=0)
     days = models.IntegerField(default=0)
-    created_by = models.ForeignKey(User)
+
 
 
 
